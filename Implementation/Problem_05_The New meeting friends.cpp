2f638ebaed20778150b6f31d036918f2e99e4c43
himanshups <74396707+himanshups@users.noreply.github.com>
2024-03-05 17:34:27 +0530

    Create Problem_05_The New meeting friends.cpp

---
+++ Implementation/Problem_05_The New meeting friends.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[3];
+    for(int i = 0;i<3;i++) cin>>arr[i];
+    sort(arr,arr+3);
+    int ans = (arr[2]-arr[1]) + (arr[1]-arr[0]);
+    cout<<ans;
+    return 0;
+}
